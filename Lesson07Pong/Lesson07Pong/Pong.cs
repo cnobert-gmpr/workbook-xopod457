369fdeff25725deaecccbfbc9c762c767b327bb9
xopod457 <xopod457@gmail.com>
2026-02-03 11:29:59 -0700

    Lesson 7
    Lesson 7

---
+++ Lesson07Pong/Lesson07Pong/Pong.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lesson07Pong;
+
+public class Pong : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private const int _WindowWidth = 750, _WindowHeight = 450, _BallWidthandHeight = 21, _PlayAreaEdgeWidth = 12;
+    private Texture2D _backgroundTexture, _ballTexture;
+    private Vector2 _ballPosition, _ballDirection;
+    private float _ballSpeed;
+    
+    internal Rectangle PlayAreaBoundingBox
+
+    {
+        get => new Rectangle(0, 0, _WindowWidth, _WindowHeight);
+    }
+
+    public Pong()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = _WindowWidth;
+        _graphics.PreferredBackBufferHeight = _WindowHeight;
+        _graphics.ApplyChanges();
+
+        _ballPosition.X = 150;
+        _ballPosition.Y = 195;
+
+        _ballSpeed = 60;
+        _ballDirection = new Vector2(-1, -1);
+
+
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _backgroundTexture = Content.Load<Texture2D>("Court");
+        _ballTexture = Content.Load<Texture2D>("Ball");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _ballPosition += _ballDirection * _ballSpeed * dt;
+
+        if(_ballPosition.X <= PlayAreaBoundingBox.Left || _ballPosition.X + _BallWidthandHeight >= PlayAreaBoundingBox.Right)
+        {
+            _ballDirection.X *= -1;
+        }
+        if (_ballPosition.Y <= PlayAreaBoundingBox.Top || _ballPosition.Y + _BallWidthandHeight >= PlayAreaBoundingBox.Bottom)
+        {
+            _ballDirection.Y *= -1;
+        }
+
+        base.Update(gameTime);
+    }
+    
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, _WindowWidth, _WindowHeight), Color.White);
+
+        Rectangle ballRectangle = new Rectangle((int)_ballPosition.X, (int)_ballPosition.Y, _BallWidthandHeight, _BallWidthandHeight);
+        _spriteBatch.Draw(_ballTexture, ballRectangle, Color.White);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
